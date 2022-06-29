e162ba1f3a8f944fc41724c155fb25890bb0c2c8
CN\Mace Y Li <mace.y.li@cn.pwc.com>
2022-06-29 16:46:34 +0800

    测试data数据补充 readme.md更新

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# easyExcel
+# easyExcelJs
 简单的操作生成漂亮的EXCEL，快速上手。提供漂亮模板直接使用
+
+
+
+## npm install
+
+```shell
+npm install easyexceljs -S
+npm install exceljs -S
+npm install file-saver -S
+```
+
+## 快速开始  以vue项目里使用为例
+```js
+import {createExcelByOneSheet,ExcelStyleTemplate,getCellPosLetter} from "easyexceljs"
+
+//用例数据  github上已提供
+import headArray from "./expmaleDate/headarray";
+import bodyarray from "./expmaleDate/bodyarray";
+
+methods:{
+    createExcelExpmale(){
+        //创建一个实例
+        const _createExcelByOneSheet = new createExcelByOneSheet(excelOptions);
+    }
+}
+
+
+```
---
+++ expmale/testData/headarray.js
@@ -0,0 +1,97 @@
+ 
+var headArray = [ 
+    {
+        "field": "type",
+        "title": "Type",
+        "width": "90"
+    },
+    {
+        "field": "metric",
+        "title": "title Name",
+        "width": "350"
+    },
+    {
+        "field": "owner",
+        "title": "Owner Name",
+        "width": "130"
+    },
+    {
+        "field": "target",
+        "title": "Target",
+        "width": "70"
+    },
+    {
+        "field": "Mar week1",
+        "title": "Mar week1",
+        "width": "100"
+    },
+    {
+        "field": "Mar week2",
+        "title": "Mar week2",
+        "width": "100"
+    },
+    {
+        "field": "Mar week3",
+        "title": "Mar week3",
+        "width": "100"
+    },
+    {
+        "field": "Mar week4",
+        "title": "Mar week4",
+        "width": "100",
+    },
+    {
+        "field": "Apr week1",
+        "title": "Apr week1",
+        "width": "100"
+    },
+    {
+        "field": "Apr week2",
+        "title": "Apr week2",
+        "width": "100"
+    },
+    {
+        "field": "Apr week3",
+        "title": "Apr week3",
+        "width": "100"
+    },
+    {
+        "field": "Apr week4",
+        "title": "Apr week4",
+        "width": "100"
+    },
+    {
+        "field": "May week1",
+        "title": "May week1",
+        "width": "100"
+    },
+    {
+        "field": "May week2",
+        "title": "May week2",
+        "width": "100"
+    },
+    {
+        "field": "May week3",
+        "title": "May week3",
+        "width": "100"
+    },
+    {
+        "field": "May week4",
+        "title": "May week4",
+        "width": "100"
+    },
+    {
+        "field": "May week5",
+        "title": "May week5",
+        "width": "100"
+    },
+    {
+        "field": "Jun week1",
+        "title": "Jun week1",
+        "width": "100"
+    }
+]
+
+
+
+export default headArray
